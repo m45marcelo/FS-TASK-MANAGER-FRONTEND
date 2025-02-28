c304beb30c608b92ebdb69ead845bac9cd1f272f
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-02-27 22:41:28 -0300

    "feat: fetch tasks from back-end"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
+
+import TaskItem from "./components/TaskItem";
+import { useEffect } from "react";
+
 function App() {
-  return <></>;
+  const [tasks, setTasks] = useState([])
+
+  const fetchTasks = async() =>{
+    try {
+      const { data } = await axios.get("http://localhost:8000/tasks")
+      setTasks(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchTasks();
+  },[])
+
+
+  return (
+    <>
+      {tasks.map((task)=>{
+        return <TaskItem key={task.id} task={task}/>
+    })}
+    </>
+  );
 }
 
 export default App;
